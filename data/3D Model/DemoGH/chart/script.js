a9e4168675029371adbffeeaa2e1add237d85220
snjsomnath <53001297+snjsomnath@users.noreply.github.com>
2021-11-17 21:22:07 +0100

    Added Charts

---
+++ data/3D Model/DemoGH/chart/script.js
@@ -0,0 +1,366 @@
+
+var data = [
+    {
+        color: "#FFFFFF", 
+        id: "0", 
+        parent: "", 
+        name: " "
+    }, 
+    {
+        color: "#fec89aff", 
+        id: "B0", 
+        parent: "0", 
+        name: "Block 1"
+    }, 
+    {
+        color: "#ffd7baff", 
+        id: "B1", 
+        parent: "0", 
+        name: "Block 2"
+    }, 
+    {
+        color: "#ffe5d9ff", 
+        id: "B2", 
+        parent: "0", 
+        name: "Block 3"
+    }, 
+    {
+        color: "#ece4dbff", 
+        id: "B3", 
+        parent: "0", 
+        name: "Block 4"
+    }, 
+    {
+        color: "#d8e2dcff", 
+        id: "B4", 
+        parent: "0", 
+        name: "Block 5"
+    }, 
+    {
+        color: "#e8e8e4ff", 
+        id: "B5", 
+        parent: "0", 
+        name: "Block 6"
+    }
+,
+    {
+        "value": 2920, 
+        "parent": "B0", 
+        "name": "unit 0"
+    }, 
+    {
+        "value": 3650, 
+        "parent": "B0", 
+        "name": "unit 1"
+    }, 
+    {
+        "value": 9624, 
+        "parent": "B0", 
+        "name": "unit 2"
+    }, 
+    {
+        "value": 8020, 
+        "parent": "B0", 
+        "name": "unit 3"
+    }, 
+    {
+        "value": 2920, 
+        "parent": "B0", 
+        "name": "unit 4"
+    }, 
+    {
+        "value": 4380, 
+        "parent": "B0", 
+        "name": "unit 5"
+    }, 
+    {
+        "value": 9624, 
+        "parent": "B0", 
+        "name": "unit 6"
+    }, 
+    {
+        "value": 6416, 
+        "parent": "B0", 
+        "name": "unit 7"
+    }, 
+    {
+        "value": 5090, 
+        "parent": "B1", 
+        "name": "unit 1"
+    }, 
+    {
+        "value": 6108, 
+        "parent": "B1", 
+        "name": "unit 2"
+    }, 
+    {
+        "value": 5090, 
+        "parent": "B1", 
+        "name": "unit 3"
+    }, 
+    {
+        "value": 8720, 
+        "parent": "B1", 
+        "name": "unit 4"
+    }, 
+    {
+        "value": 6972, 
+        "parent": "B1", 
+        "name": "unit 5"
+    }, 
+    {
+        "value": 6108, 
+        "parent": "B1", 
+        "name": "unit 6"
+    }, 
+    {
+        "value": 4072, 
+        "parent": "B1", 
+        "name": "unit 7"
+    }, 
+    {
+        "value": 3054, 
+        "parent": "B1", 
+        "name": "unit 8"
+    }, 
+    {
+        "value": 10900, 
+        "parent": "B1", 
+        "name": "unit 9"
+    }, 
+    {
+        "value": 7000, 
+        "parent": "B2", 
+        "name": "unit 1"
+    }, 
+    {
+        "value": 3768, 
+        "parent": "B2", 
+        "name": "unit 2"
+    }, 
+    {
+        "value": 7000, 
+        "parent": "B2", 
+        "name": "unit 3"
+    }, 
+    {
+        "value": 2512, 
+        "parent": "B2", 
+        "name": "unit 4"
+    }, 
+    {
+        "value": 8400, 
+        "parent": "B2", 
+        "name": "unit 5"
+    }, 
+    {
+        "value": 3768, 
+        "parent": "B2", 
+        "name": "unit 6"
+    }, 
+    {
+        "value": 5600, 
+        "parent": "B2", 
+        "name": "unit 7"
+    }, 
+    {
+        "value": 6350, 
+        "parent": "B3", 
+        "name": "unit 1"
+    }, 
+    {
+        "value": 7620, 
+        "parent": "B3", 
+        "name": "unit 2"
+    }, 
+    {
+        "value": 6350, 
+        "parent": "B3", 
+        "name": "unit 3"
+    }, 
+    {
+        "value": 10736, 
+        "parent": "B3", 
+        "name": "unit 4"
+    }, 
+    {
+        "value": 8484, 
+        "parent": "B3", 
+        "name": "unit 5"
+    }, 
+    {
+        "value": 7620, 
+        "parent": "B3", 
+        "name": "unit 6"
+    }, 
+    {
+        "value": 5080, 
+        "parent": "B3", 
+        "name": "unit 7"
+    }, 
+    {
+        "value": 3810, 
+        "parent": "B3", 
+        "name": "unit 8"
+    }, 
+    {
+        "value": 13420, 
+        "parent": "B3", 
+        "name": "unit 9"
+    }, 
+    {
+        "value": 5100, 
+        "parent": "B4", 
+        "name": "unit 1"
+    }, 
+    {
+        "value": 2628, 
+        "parent": "B4", 
+        "name": "unit 2"
+    }, 
+    {
+        "value": 5100, 
+        "parent": "B4", 
+        "name": "unit 3"
+    }, 
+    {
+        "value": 1752, 
+        "parent": "B4", 
+        "name": "unit 4"
+    }, 
+    {
+        "value": 6120, 
+        "parent": "B4", 
+        "name": "unit 5"
+    }, 
+    {
+        "value": 2628, 
+        "parent": "B4", 
+        "name": "unit 6"
+    }, 
+    {
+        "value": 4080, 
+        "parent": "B4", 
+        "name": "unit 7"
+    }, 
+    {
+        "value": 3500, 
+        "parent": "B5", 
+        "name": "unit 1"
+    }, 
+    {
+        "value": 5064, 
+        "parent": "B5", 
+        "name": "unit 2"
+    }, 
+    {
+        "value": 3500, 
+        "parent": "B5", 
+        "name": "unit 3"
+    }, 
+    {
+        "value": 6176, 
+        "parent": "B5", 
+        "name": "unit 4"
+    }, 
+    {
+        "value": 4200, 
+        "parent": "B5", 
+        "name": "unit 5"
+    }, 
+    {
+        "value": 4200, 
+        "parent": "B5", 
+        "name": "unit 6"
+    }, 
+    {
+        "value": 3376, 
+        "parent": "B5", 
+        "name": "unit 7"
+    }, 
+    {
+        "value": 2100, 
+        "parent": "B5", 
+        "name": "unit 8"
+    }, 
+    {
+        "value": 7720, 
+        "parent": "B5", 
+        "name": "unit 9"
+    }
+];
+
+
+Highcharts.setOptions({
+  colors: ["#ecb37c", "#ECE100"]
+});
+Highcharts.chart("container", {
+  chart: {
+    height: "100%"
+  },
+
+  title: {
+    text: "Summary of BTA"
+  },
+  subtitle: {
+    text:
+      'Summary of data generated by Blocktool'
+  },
+  series: [
+    {
+      type: "sunburst",
+      data: data,
+      allowDrillToNode: true,
+      cursor: "pointer",
+      borderWidth: 3,
+      borderColor: "#000000",
+      dataLabels: {
+        format: "{point.name}",
+        filter: {
+          property: "innerArcLength",
+          operator: ">",
+          value: 16
+        },
+        style: {
+          textOutline: false,
+          color: "#000000"
+        }
+      },
+      levels: [
+        {
+          level: 1,
+          levelIsConstant: false,
+          dataLabels: {
+            filter: {
+              property: "outerArcLength",
+              operator: ">",
+              value: 64
+            }
+          }
+        },
+        {
+          level: 2,
+          colorByPoint: true
+        },
+        {
+          level: 3,
+          colorVariation: {
+            key: "brightness",
+            to: -0.5
+          }
+        },
+        {
+          level: 4,
+          colorVariation: {
+            key: "brightness",
+            to: 0.5
+          }
+        }
+      ]
+    }
+  ],
+  tooltip: {
+    headerFormat: "",
+    pointFormat: "The area of <b>{point.name}</b> is <b>{point.value}</b> kvm"
+  }
+});
